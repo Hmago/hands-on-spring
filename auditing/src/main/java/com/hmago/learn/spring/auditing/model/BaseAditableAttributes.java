eea7dd94b778b917008f06b2cc8fc12115c3430b
Harshit Mago <Harshit@Kartics-MacBook-Pro.local>
2019-12-30 00:18:54 +0530

    optimizing imports

--- auditing/src/main/java/com/hmago/learn/spring/auditing/model/BaseAditableAttributes.java
+++ auditing/src/main/java/com/hmago/learn/spring/auditing/model/BaseAditableAttributes.java
@@ -5,21 +5,17 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.domain.Persistable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 
 @Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseAditableAttributes<T>  {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+public abstract class BaseAditableAttributes<T> {
 
     @CreatedBy
     T createdBy;
--- auditing/src/main/java/com/hmago/learn/spring/auditing/model/Product.java
+++ auditing/src/main/java/com/hmago/learn/spring/auditing/model/Product.java
@@ -3,13 +3,12 @@ package com.hmago.learn.spring.auditing.model;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 
 @Data
 @Entity
-@Table(name="product")
+@Table(name = "product")
 @NoArgsConstructor
 public class Product extends BaseAditableAttributes<String> {
 
@@ -26,8 +25,7 @@ public class Product extends BaseAditableAttributes<String> {
     private int enabled;
 
     @Builder(builderMethodName = "builder")
-    public Product(Long id, String name, String category, double price, int enabled) {
-        this.id = id;
+    public Product(String name, String category, double price, int enabled) {
         this.name = name;
         this.category = category;
         this.price = price;
